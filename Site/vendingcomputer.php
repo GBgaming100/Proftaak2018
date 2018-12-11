d8770d42fea4c786d52e2a08b610ec64ac096f17
Maarten <maarten.jakobs@gmail.com>
2018-12-11 14:24:40 +0100

    Scanner gefixt
    nu doet de kinect het wel

--- Site/vendingcomputer.php
+++ Site/vendingcomputer.php
@@ -40,8 +40,44 @@
 
     <?php importHeader("scrolled") ?>
 
-    <button class="btn btn-secondary js-scan">scan</button>
-	<video id="preview" autoplay="autoplay" class="active" src="" style="transform: scaleX(-1);"></video>
+             <div id="QR-Code" class="container" style="width:100%">
+            <div class="panel panel-primary">
+                <div class="panel-heading" style="display: inline-block;width: 100%;">
+                    <h4 style="width:50%;float:left;">WebCodeCam.js Demonstration</h4>
+                    <div style="width:50%;float:right;margin-top: 5px;margin-bottom: 5px;text-align: right;">
+                    <select id="cameraId" class="form-control" style="display: inline-block;width: auto;"></select>
+                    <button id="save" data-toggle="tooltip" title="Image shoot" type="button" class="btn btn-info btn-sm disabled"><span class="glyphicon glyphicon-picture"></span></button>
+                    <button id="play" data-toggle="tooltip" title="Play" type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-play"></span></button>
+                    <button id="stop" data-toggle="tooltip" title="Stop" type="button" class="btn btn-warning btn-sm"><span class="glyphicon glyphicon-stop"></span></button>
+                    <button id="stopAll" data-toggle="tooltip" title="Stop streams" type="button" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-stop"></span></button>
+                </div>
+            </div>
+            <div class="panel-body">
+                <div class="col-md-6" style="text-align: center;">
+                    <div class="well" style="position: relative;display: inline-block;">
+                        <canvas id="qr-canvas" width="320" height="240"></canvas>
+                        <div class="scanner-laser laser-rightBottom" style="opacity: 0.5;"></div>
+                        <div class="scanner-laser laser-rightTop" style="opacity: 0.5;"></div>
+                        <div class="scanner-laser laser-leftBottom" style="opacity: 0.5;"></div>
+                        <div class="scanner-laser laser-leftTop" style="opacity: 0.5;"></div>
+                    </div>
+                </div>
+                <div class="col-md-6" style="text-align: center;">
+                    <div id="result" class="thumbnail">
+                        <div class="well" style="position: relative;display: inline-block;">
+                            <img id="scanned-img" src="" width="320" height="240">
+                        </div>
+                        <div class="caption">
+                            <h3>Scanned result</h3>
+                            <p id="scanned-QR"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-footer">
+            </div>
+        </div>
+    </div>
 
 </body>
 <?php 
@@ -49,4 +85,6 @@
     js();
 
 ?>
+<script type="text/javascript" src="js/qrcodelib.js"></script>
+<script type="text/javascript" src="js/WebCodeCam.js"></script>
 <script src="js/vendingcomputer.js"></script>
